694a927ce69017846ff608fed477471e42cb5672
ArchiePerera <archivaldoperera@gmail.com>
2022-04-25 18:15:47 +0200

    multer implentation

--- app.js
+++ app.js
@@ -1,43 +1,32 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const userRoutes = require('./routes/user');
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/user");
 
-mongoose.connect('mongodb+srv://Archie:Openclassrooms@cluster0.j3xkx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose
+  .connect(
+    "mongodb+srv://Archie:Openclassrooms@cluster0.j3xkx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // * signifie : depuis n'importe quelle origine
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // Autorise les méthodes de communication GET/POST/PUT...
-    next();
-  });
-
-  app.use('/api/auth', userRoutes);
-
-app.use((req, res, next) => {
-  console.log('Requête reçue !');
+  res.setHeader("Access-Control-Allow-Origin", "*"); // * signifie : depuis n'importe quelle origine
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  ); // Autorise les méthodes de communication GET/POST/PUT...
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
