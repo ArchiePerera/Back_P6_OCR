cd24f4336f8b3869c4bc25f6e223d7578e55d87b
ArchiePerera <archivaldoperera@gmail.com>
2022-04-27 09:26:51 +0200

    sauce path : getOne

--- routes/sauce.js
+++ routes/sauce.js
@@ -8,4 +8,6 @@ const router = express.Router();
 router.get('/', auth, sauceCtrl.getAllSauces);
 router.post('/', auth, multer, sauceCtrl.createSauce);
 
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+
 module.exports = router;
